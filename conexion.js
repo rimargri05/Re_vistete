618d80573a2798a01992bb2eab2b2cc254ba080f
vanesa <vanesyiyatasconavila@gmail.com>
2025-10-06 13:01:34 -0500

    cambiode3000puertoa8080

--- conexion.js
+++ conexion.js
@@ -84,5 +84,5 @@ app.post('/login', async (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Servidor escuchando en el puerto 3000');
+  console.log('Servidor escuchando en el puerto 8080');
 });
