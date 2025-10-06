227ded1faed24f89f632dacf2be32729273bf726
vanesa <vanesyiyatasconavila@gmail.com>
2025-10-06 00:49:33 -0500

    intento12

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.post('/ingresar', (req, res) => {
 });
 
 // Iniciar servidor
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
